93e8d16ba2ffb90ec59aeedf9fa0b62fd86b3c82
luciussulla <maciek.kowalczyk@yandex.com>
2021-11-01 17:10:30 +0100

    Style website

---
+++ public/admin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="styles/style.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="header">
+    <h1>Widget Corp</h1>
+  </div>
+  <div class="main">
+    <div class="navigation">
+      
+    </div>
+    <div class="page">
+      <h2>Admin Menu</h2>
+      <p>Welcome to the admin area</p>
+      <ul class="pages">
+        <li><a href="admin.php">admin</a></li>
+        <li><a href="manage_content.php">manage content</a></li>
+        <li><a href="manage_admins.php">manage admins</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="footer">
+    Copyright &copy; 2021
+  </div>
+</body>
+</html> 
--- public/index.php
+++ public/index.php
@@ -7,6 +7,25 @@
   <title>Document</title>
 </head>
 <body>
-  
+  <div class="header">
+    <h1>Widget Corp</h1>
+  </div>
+  <div class="main">
+    <div class="navigation">
+
+    </div>
+    <div class="page">
+      <h2>Admin Menu</h2>
+      <p>Welcome to the admin area</p>
+      <ul>
+        <li><a href="admin.php">admin</a></li>
+        <li><a href="manage_content.php">manage content</a></li>
+        <li><a href="manage_admins.php">manage admins</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="footer">
+    Copyright &copy; 2021
+  </div>
 </body>
 </html>
