c82d3930ccb39affab6e6e1336571ebafca521e3
luciussulla <maciek.kowalczyk@yandex.com>
2021-11-09 18:21:03 +0100

    Make side navigation dynamic

--- includes/functions.php
+++ includes/functions.php
@@ -6,6 +6,28 @@
   }
 
   function find_all_subjects() {
-    
+    global $connection;
+
+    $query = "SELECT * ";
+    $query .= "FROM subjects ";
+    $query .= "WHERE visible = 1 ";
+    $query .= "ORDER BY position ASC"; 
+    $subject_set = mysqli_query($connection, $query); 
+    confirm_query($subject_set);
+    return $subject_set;
   }
+
+  function find_all_pages($subject_id) {
+    global $connection; 
+
+    $query = "SELECT * ";
+    $query .= "FROM pages ";
+    $query .= "WHERE visible = 1 ";
+    $query .= "AND subject_id = {$subject_id} ";
+    $query .= "ORDER BY position ASC"; 
+    $page_set = mysqli_query($connection, $query); 
+    confirm_query($page_set);  
+    return $page_set;
+  }  
+
 ?>
--- public/manage_content.php
+++ public/manage_content.php
@@ -1,46 +1,60 @@
 <?php require_once('../includes/dbconnection.php');?>
 <?php require_once('../includes/functions.php');?>
-
 <?php include('../includes/layouts/header.php');?>
+
+<?php 
+  if (isset($_GET["subject"])) {
+    $selected_subject_id = $_GET["subject"];
+    $selected_page_id = null;
+  } elseif(isset($_GET["page"])) {
+    $selected_page_id = $_GET["page"];
+    $selected_subject_id = null; 
+  } else {
+    $selected_subject_id = null; 
+    $selected_page_id = null;
+  }
+?>
+
   <div class="main"> 
     <div class="navigation"> 
       <ul class="subjects">
-        <?php 
-          $query = "SELECT * ";
-          $query .= "FROM subjects ";
-          $query .= "WHERE visible = 1 ";
-          $query .= "ORDER BY position ASC"; 
-          $subject_set = mysqli_query($connection, $query); 
-          confirm_query($subject_set);
-        ?>
-
+        <?php $subject_set = find_all_subjects(); ?> 
+        
         <?php while($subject = mysqli_fetch_assoc($subject_set)) { ?>      
-          <li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?>
             <?php 
-              $query = "SELECT * ";
-              $query .= "FROM pages ";
-              $query .= "WHERE visible = 1 ";
-              $query .= "AND subject_id = {$subject["id"]} ";
-              $query .= "ORDER BY position ASC"; 
-              $page_set = mysqli_query($connection, $query); 
-              confirm_query($page_set);  
+             echo "<li ";
+             if($subject["id"]==$selected_subject_id) {
+               echo "class=\"selected\"";
+             }  
+             echo ">";
             ?>
+            <a href="manage_content.php?subject=<?php echo $subject["id"]?>"><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")";?></a>
+            <?php $page_set = find_all_pages($subject["id"]); ?>
+
             <ul class="pages">
                <?php while($page = mysqli_fetch_assoc($page_set)) { ?>
-                 <li><?php echo $page["menu_name"]; ?></li>
+                  <?php 
+                    echo "<li";
+                    if($page["id"]==$selected_page_id) {
+                      echo " class=\"selected\"";
+                    }
+                    echo ">";
+                  ?>
+                  <a href="manage_content.php?page=<?php echo $page["id"]?>"><?php echo $page["menu_name"]; ?></a></li>
                <?php } ?> 
                <?php mysqli_free_result($page_set); ?>
             </ul>
+                
           </li>
         <?php } ?>
-
         <?php mysqli_free_result($subject_set); ?>
       </ul>
     </div>
 
-
     <div class="page">
       <h2>Manage Content</h2>
+      <?php echo $selected_page_id ?>
+      <?php echo $selected_subject_id ?>
     </div>
   </div>
 
